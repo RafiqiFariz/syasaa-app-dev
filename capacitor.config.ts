22271ce2c2ed7a23ea0e92533bd2e56d29bce2d4
FarizRafiqi <auliaelihza@gmail.com>
2024-02-25 20:56:55 +0700

    feat: change app name and menu

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,20 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Syasaa-App',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
+    appId: 'io.ionic.starter',
+    appName: 'Syasaa-App',
+    webDir: 'dist',
+    server: {
+        androidScheme: 'https'
+    },
+    plugins: {
+        LiveUpdates: {
+            appId: '41d6a410',
+            channel: 'Production',
+            autoUpdateMethod: 'background',
+            maxVersions: 2
+        }
+    }
 };
 
 export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -57,15 +57,15 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>My Absence</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel>Profile</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
