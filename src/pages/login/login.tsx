0ec1caad488720b0c0a0f68c71f92dcf4fa60116
Natan <zantanad.24@gmail.com>
2024-03-03 23:04:16 +0700

    update form sign in and sign up

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,73 +2,66 @@ import { Button, Card, Checkbox, Form, Input } from "antd"
 import styles from './login.module.css'
 import { LockOutlined, UserOutlined } from "@ant-design/icons"
 import { IonItem } from "@ionic/react"
+import { useState } from "react"
 export const LoginPage = ()=>{
-    const onFinish = (values: any) => {
-        console.log('Received values of form: ', values);
+    const [form, setFrom] = useState({
+        username: '',
+        password: '',
+
+    
+    })
+    const handleChange = (event: any) => {
+        const {name, value} = event.target
+        console.log(event.target.name)
+        setFrom({
+            ...form,
+            [name]: value
+        })
+    }
+    const onFinish = (event: any) => {
+        event.preventDefault()
+        console.log('Received values of form: ', form);
       };
     return (
-        <div className={styles.layout}>
-             <Card className={styles.card}>
-             <Form
-                name="normal_login"
-                className="login-form"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                >
-                <Form.Item
-                    className="text-primary"
-                    name="username"
-                    rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Username!',
-                    },
-                    ]}
-                >
-                    <Input prefix={<i className="bi bi-person-fill" style={{color:'grey'}}></i>} placeholder="Username" />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Password!',
-                    },
-                    ]}
-                >
-                    <Input
-                    prefix={<i className="bi bi-lock-fill" style={{color:'grey'}}></i>}
-                    type="password"
-                    placeholder="Password"
-                    />
-                </Form.Item>
-                <Form.Item>
-                    <div className="d-flex justify-content-between">
-                        <Form.Item name="remember" valuePropName="checked" noStyle>
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-                        <a className="login-form-forgot" href="">
-                            Forgot password
-                        </a>
-                    </div>
-                </Form.Item>
-
-                <Form.Item>
-                    <div className="d-flex flex-column">
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
-                        </Button>
-                        <div>
-                            <IonItem routerLink="/register" routerDirection="forward">
-                                <span className="text-primary">Or register now!</span>
-                            </IonItem>
-                        </div>
-                    </div>
-                </Form.Item>
-                </Form>
-            </Card>
+    <div className="container my-auto mt-5">
+        <div className="row">
+          <div className="col-lg-4 col-md-8 col-12 mx-auto">
+            <div className="card z-index-0 fadeIn3 fadeInBottom">
+              <div className="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div className="bg-primary shadow-primary border-radius-lg py-3 pe-1 mt-3">
+                  <h4 className="text-white font-weight-bolder text-center mt-2 mb-0">Absensi Syasa</h4> 
+                </div>
+              </div>
+              <div className="card-body">
+                <form onSubmit={onFinish}>
+                <div className="input-group input-group-dynamic mb-4">
+                    <span className="input-group-text" id="basic-addon1">
+                        <i className="bi bi-person-fill"></i>
+                    </span>
+                    <input name="username" value={form.username} onChange={handleChange} type="email" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                </div>
+                <div className="input-group input-group-dynamic mb-4">
+                    <span className="input-group-text" id="basic-addon2">
+                        <i className="bi bi-person-lock"></i>
+                    </span>
+                    <input name="password" value={form.password} onChange={handleChange} type="password" className="form-control" placeholder="password" aria-label="Password" aria-describedby="basic-addon2"/>
+                </div>
+                  <div className="form-check form-switch d-flex align-items-center mb-3">
+                    <input className="form-check-input" type="checkbox" id="rememberMe"/>
+                    <label className="form-check-label mb-0 ms-3" >Remember me</label>
+                  </div>
+                  <div className="text-center">
+                    <button type="submit" className="btn bg-primary w-100 my-4 mb-2 text-white">Sign in</button>
+                  </div>
+                  <p className="mt-4 text-sm text-center">
+                    Don't have an account?
+                    <IonItem className="text-primary text-gradient font-weight-bold" routerLink="/register">Sign up</IonItem>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
+    </div>
     )
 }
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,78 +1,70 @@
 import { Button, Card, Form, Input } from 'antd';
 import styles from './register.module.css';
+import { IonItem } from '@ionic/react';
+import { useState } from 'react';
 
 export const RegisterPage = ()=>{
-    const onFinish = (values: any) => {
-        console.log('Received values of form: ', values);
+    const [form, setFrom] = useState({
+        username: '',
+        password: '',
+        confirmPassword: ''
+    
+    })
+    const handleChange = (event: any) => {
+        const {name, value} = event.target
+        console.log(event.target.name)
+        setFrom({
+            ...form,
+            [name]: value
+        })
+    }
+    const onFinish = (event: any) => {
+        event.preventDefault()
+        console.log('Received values of form: ', form);
+        // setFrom(values)
       };
     return(
-        <div className={styles.layout}>
-             <Card className={styles.card}>
-             <Form
-                name="normal_register"
-                className="login-register"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                >
-                    <Form.Item
-                        name="email"
-                        rules={[
-                            {
-                              type: 'email',
-                              message: 'The input is not valid E-mail!',
-                            },
-                            {
-                              required: true,
-                              message: 'Please input your E-mail!',
-                            },
-                          ]}
-                    >
-                        <Input prefix={<i className="bi bi-person-fill" style={{color:'grey'}}></i>} placeholder="Username" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your Password!' }]}
-                    >
-                        <Input
-                        prefix={<i className="bi bi-file-lock-fill" style={{color:'grey'}}></i>}
-                        type="password"
-                        placeholder="Password"
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="confirm_password"
-                        rules={[
-                            {
-                              required: true,
-                              message: 'Please confirm your password!',
-                            },
-                            ({ getFieldValue }) => ({
-                              validator(_, value) {
-                                if (!value || getFieldValue('password') === value) {
-                                  return Promise.resolve();
-                                }
-                                return Promise.reject(new Error('The new password that you entered do not match!'));
-                              },
-                            }),
-                          ]}
-                    >
-                        <Input
-                        prefix={<i className="bi bi-lock-fill" style={{color:'grey'}}></i>}
-                        type="password"
-                        placeholder="confirm password"
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <div className="d-flex flex-column">
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                Register
-                            </Button>
-                        </div>
-                    </Form.Item>
-                </Form>
-            </Card>
+      <div className="container my-auto mt-5">
+      <div className="row">
+        <div className="col-lg-4 col-md-8 col-12 mx-auto">
+          <div className="card z-index-0 fadeIn3 fadeInBottom">
+            <div className="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+              <div className="bg-primary shadow-primary border-radius-lg py-3 pe-1 mt-3">
+                <h4 className="text-white font-weight-bolder text-center mt-2 mb-0">Sign Up Syasa</h4> 
+              </div>
+            </div>
+            <div className="card-body">
+              <form onSubmit={onFinish}>
+              <div className="input-group input-group-dynamic mb-4">
+                  <span className="input-group-text" id="basic-addon1">
+                      <i className="bi bi-person-fill"></i>
+                  </span>
+                  <input name='username' value={form.username} onChange={handleChange} type="email" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+              <div className="input-group input-group-dynamic mb-4">
+                  <span className="input-group-text" id="basic-addon2">
+                      <i className="bi bi-person-lock"></i>
+                  </span>
+                  <input name='password' value={form.password} onChange={handleChange} type="password" className="form-control" placeholder="password" aria-label="Password" aria-describedby="basic-addon2"/>
+              </div>
+              <div className="input-group input-group-dynamic mb-4">
+                  <span className="input-group-text" id="basic-addon3">
+                    <i className="bi bi-key-fill"></i>
+                  </span>
+                  <input name='confirmPassword' value={form.confirmPassword} onChange={handleChange} type="password" className="form-control" placeholder="Confirm Password" aria-label="Confirm Password" aria-describedby="basic-addon3"/>
+              </div>
+                <div className="text-center">
+                  <button type="submit" className="btn bg-primary w-100 my-4 mb-2 text-white">Sign up</button>
+                </div>
+                <p className="mt-4 text-sm text-center">
+                   have an account?
+                  <IonItem className="text-primary text-gradient font-weight-bold" routerLink="/login">Sign in</IonItem>
+                </p>
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
+  </div>
     )
 }
