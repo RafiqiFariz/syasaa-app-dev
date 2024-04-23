3c0b93466ce1b9f700a8dc403555beb6694d3873
Natan <zantanad.24@gmail.com>
2024-04-23 07:37:19 +0700

    add profile nav

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -16,6 +16,7 @@ export const UserLayout = ({ children }: LayoutProps) => {
   const history = useHistory();
 
   const { isLogin, setIsLogin } = useContext(AuthContext);
+  const [user, setUser] = useState<any>({});
 
   const [nav, setNav] = useState(false);
 
@@ -66,8 +67,12 @@ export const UserLayout = ({ children }: LayoutProps) => {
     }
   };
 
-  useEffect(() => {}, []);
-
+  useEffect(() => {
+    if (isLogin.data) {
+      setUser(isLogin.data);
+    }
+  }, [isLogin.data]);
+  console.log(user, "data");
   return (
     <div
       className={`g-sidenav-show bg-gray-200 ${
@@ -115,16 +120,11 @@ export const UserLayout = ({ children }: LayoutProps) => {
                 </h6>
               </nav>
               <div
-                className="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4"
+                className="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex items-end"
                 id="navbar"
               >
-                <div className="ms-md-auto pe-md-3 d-flex align-items-center">
-                  <div className="input-group input-group-outline">
-                    <label className="form-label">Type here...</label>
-                    <input type="text" className="form-control" />
-                  </div>
-                </div>
-                <ul className="navbar-nav  justify-content-end">
+                <div className="ms-md-auto pe-md-3 d-flex align-items-center"></div>
+                <ul className="navbar-nav justify-content-end">
                   <li className="nav-item d-xl-none ps-3 d-flex align-items-center mx-3">
                     <a
                       onClick={() => setNav((prev) => !prev)}
@@ -139,14 +139,62 @@ export const UserLayout = ({ children }: LayoutProps) => {
                     </a>
                   </li>
                   <li className="nav-item d-flex align-items-center">
-                    <button
+                    <div
+                      className="collapse navbar-collapse"
+                      id="navbarNavDarkDropdown"
+                    >
+                      <ul className="navbar-nav">
+                        <li className="nav-item dropdown">
+                          <div
+                            className="nav-link d-flex flex-row"
+                            // href="#"
+                            id="navbarDarkDropdownMenuLink"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                          >
+                            <span className="nav-link-text d-flex justify-content-center align-items-center text-md text-bold mx-2">
+                              {user && user.name ? user.name : "Guest"}
+                            </span>
+                            <img
+                              src="https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png"
+                              alt="user"
+                              className="avatar avatar-sm me-2"
+                              style={{ borderRadius: "50%" }}
+                            />
+                          </div>
+                          <ul
+                            className="dropdown-menu "
+                            aria-labelledby="navbarDarkDropdownMenuLink"
+                          >
+                            <li>
+                              <button
+                                className="dropdown-item"
+                                onClick={handleSignOut}
+                              >
+                                Sign Out
+                              </button>
+                            </li>
+                            <li>
+                              <button
+                                className="dropdown-item"
+                                onClick={() => history.push("/profile")}
+                              >
+                                Profile
+                              </button>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </div>
+                    {/* <button
                       className="nav-link text-body font-weight-bold px-0"
                       data-bs-toggle="modal"
                       data-bs-target="#exampleModal"
                       onClick={handleSignOut}
-                    >
-                      <span className="d-sm-inline d-none">Sign Out</span>
-                    </button>
+                    > */}
+                    {/* <span className="d-sm-inline d-none">Sign Out</span> */}
+                    {/* </button> */}
                   </li>
                 </ul>
               </div>
